9c4d42d3d8b40831594ff2587f377b05c2cb134a
Bbbgl <104217088+Bbbgl@users.noreply.github.com>
2022-12-15 13:07:23 +0100

    Add files via upload

---
+++ Library/Library/Solution.Library/BusinessLogic.Library/ILibraryBusinessLogic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Library
+{
+    public interface ILibraryBusinessLogic
+    {
+        //User Login(string username, string password);
+
+        //void AddBook(Book book);
+
+        //List<Book> SearchBook(Book book);
+
+        //List<BookViewModel> SearchBookWithAvailabilityInfos(Book book);
+
+        //void UpdateBook(int bookId, Book bookWithNewValues);
+
+        //void DeleteBook(int bookId);
+
+        //User GetUserByUserName(string userName);
+
+        //ReservationResult ReserveBook(int bookId, int userId);
+
+        //ReservationResult BookReturn(int bookId, int userId);
+
+        //List<ReservationViewModel> GetReservationHistory(int? bookId, int? userId, ReservationStatus? reservationStatus);
+    }
+}
